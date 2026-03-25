c9ab8329683c5ec9b6c3ebba81a96d9c188fbd39
Kiyoto Yamanaka <kiyoymnk022@gmail.com>
2026-03-25 22:30:42 +0900

    add:カート詳細

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import type { FC } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CartPage from "./pages/CartPage";
+import CheckOutPage from "./pages/CheckoutPage";
 import HomePage from "./pages/HomePage";
+import OrderCompletePage from "./pages/OrderCompletePage";
 import ProductDetailPage from "./pages/ProductDetail";
 
 const App: FC = () => {
@@ -11,6 +13,11 @@ const App: FC = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckOutPage />} />
+        <Route
+          path="/order-complete/:orderId"
+          element={<OrderCompletePage />}
+        />
       </Routes>
     </BrowserRouter>
   );
